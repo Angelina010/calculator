e22bbb6bb675076a3f9b3a461260762b9fcfa6a7
Angelina Zhou <zhouangelina3@gmail.com>
2024-08-18 20:12:49 -0400

    Connected arithmetic logic to UI; calculator now handles addition, subtraction, multiplication, and divsion

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function multiply (num1, num2) {
 }
 
 function divide (num1, num2){
-    return num1 / num2;
+    return num2 === 0? "Error: Division by 0" : num1 / num2
 }
 
 function operate (num1, num2, operator){
@@ -20,7 +20,7 @@ function operate (num1, num2, operator){
             return add(num1, num2)
         case '-':
             return subtract(num1,num2)
-        case '*':
+        case 'x':
             return multiply(num1,num2)
         case '/':
             return divide(num1, num2)
@@ -29,6 +29,62 @@ function operate (num1, num2, operator){
     }
 }
 
-let num1;
-let num2;
-let operator;
+function updateDisplay(){
+    divDisplay.textContent = curDisplay;
+}
+
+
+let num1 = null;
+let num2 = null;
+let operator = "";
+
+let curDisplay = "";
+
+let isCalculated = false; //if true, num1 is the result of a previous calculation and not from user input
+
+const divDisplay = document.querySelector(".display");
+
+const digitBtns = document.querySelectorAll(".digit");
+digitBtns.forEach((digitBtn) => {
+    digitBtn.addEventListener("click", (event) => {
+        if (isCalculated){
+            curDisplay = event.target.textContent;
+            num1 =Number(curDisplay);
+            isCalculated = false;
+        }
+        else if (operator === ""){
+            curDisplay += event.target.textContent;
+            num1 =Number(curDisplay);
+        }
+        else{
+            curDisplay = event.target.textContent;
+            num2 =Number(curDisplay);
+        }
+        updateDisplay();
+        
+    });
+})
+
+const operatorBtns = document.querySelectorAll(".operator")
+operatorBtns.forEach((operatorBtn) => {
+    operatorBtn.addEventListener("click", (event) => {
+        if (num1 != null && num2 != null){
+            num1 = operate(num1, num2, operator)
+        }
+        operator = event.target.textContent;
+        isCalculated = false;
+    }
+)
+})
+
+
+const equalsBtn = document.querySelector(".equals")
+equalsBtn.addEventListener("click", () => {
+    curDisplay = operate(num1, num2, operator);
+    num1 = Number(curDisplay);
+    num2 = null;
+    operator = "";
+    isCalculated = true;
+    updateDisplay();
+})
+
